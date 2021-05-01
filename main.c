72e3c1fc98cb0f7253c0ca2f88c1ab4c4f640935
niteshtolani2812 <80336145+niteshtolani2812@users.noreply.github.com>
2021-05-01 10:08:31 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,30 @@
+
 #include <avr/io.h>
 #include<util/delay.h>
 
+void heatedseat_init(void)
+{
+    port();         //Activity 1
+    INitADC();      //Activity 2
+    InitPWM();      //Activity 3
+    InitUART();     //Activity 4
+}
+
 int main(void)
 {
-port();
-
+uint16_t temp;
+char temp_data;
 
     while(1)
     {
-        if(!(PIND&(1<<PD0))&&(!(PIND&(1<<PD1))))
+        if(!(PIND&(1<<PD0))&&(!(PIND&(1<<PD1))))    //if both the switches are pressed
         {
             PORTB|=(1<<PB0);
+            temp=READADC(0);
+            temp_data = OutPWM(temp);
+            UARTwrite(temp_data);
             _delay_ms(200);
+
         }
 
         else
