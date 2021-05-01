e241881e7fe585caf347d74616d36cbf1dffaa5b
niteshtolani2812 <80336145+niteshtolani2812@users.noreply.github.com>
2021-05-01 15:18:35 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,13 @@
-#include<Activity1.h>
-#include<Activity2.h>
-#include<Activity3.h>
-#include<Activity4.h>
 #include <avr/io.h>
 #include<util/delay.h>
 
-void heatedseat_init(void)
-{
-    port();         //Activity 1
-    INitADC();      //Activity 2
-    InitPWM();      //Activity 3
-    InitUART();     //Activity 4
-}
-
 int main(void)
 {
-uint16_t temp;
-char temp_data;
-
     while(1)
     {
         if(!(PIND&(1<<PD0))&&(!(PIND&(1<<PD1))))    //if both the switches are pressed
         {
             PORTB|=(1<<PB0);
-            temp=READADC(0);
-            temp_data = OutPWM(temp);
-            UARTwrite(temp_data);
             _delay_ms(200);
 
         }
